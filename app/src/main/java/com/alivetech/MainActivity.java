8bdf7f4b3cbd13a3b87fc783cdf126fe754ff40e
Sneha Wadhai <snehawadhai001@gmail.com>
2021-04-22 12:35:02 +0530

    Toas Added in MainActivity

--- app/src/main/java/com/alivetech/MainActivity.java
+++ app/src/main/java/com/alivetech/MainActivity.java
@@ -3,6 +3,7 @@ package com.alivetech;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_ll_vertical);
+
+        Toast.makeText(this, "Hii alive tech", Toast.LENGTH_SHORT).show();
     }
 
 
