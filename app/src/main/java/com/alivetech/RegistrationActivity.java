573a0a20b1d0ed47c4c9913273903d5c84430ae7
Sneha Wadhai <snehawadhai001@gmail.com>
2021-04-22 12:42:03 +0530

    Toas Added in  MainActivity

--- app/src/main/java/com/alivetech/RegistrationActivity.java
+++ app/src/main/java/com/alivetech/RegistrationActivity.java
@@ -11,6 +11,7 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
         Toast.makeText(this, "Hlo From Me", Toast.LENGTH_SHORT).show();
     }
 }
